abcf1d0fcfe91863d97945df623f6fd31d972456
andylee-24 <pinetree2100@gmail.com>
2024-06-12 11:05:15 +0900

    update: add text-filtering using nlp library

--- musiclm-pytorch/musiclm_pytorch/trainer.py
+++ musiclm-pytorch/musiclm_pytorch/trainer.py
@@ -139,7 +139,7 @@ def pad_to_longest_fn(data):
 
 def get_dataloader(ds, pad_to_longest = True, **kwargs):
     collate_fn = pad_to_longest_fn if pad_to_longest else curtail_to_shortest_collate
-    return DataLoader(ds, collate_fn = collate_fn, **kwargs)
+    return DataLoader(ds, collate_fn = collate_fn, num_workers=4, **kwargs)
 
 # semantic transformer trainer
 
